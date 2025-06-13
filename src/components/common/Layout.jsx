d49b869994f08c21da943f5f748faeeeef04bb6a
merna Tera <mernatera@live.com>
2025-06-13 17:16:31 +0300

    fixed layout backgroud

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -5,13 +5,13 @@ import Topbar from "./Topbar";
 
 const Layout = () => {
   return (
-    <div className="h-screen flex flex-col bg-[#ebebeb]">
+    <div className="h-screen flex flex-col ">
       <Topbar />
 
       <div className="flex flex-1">
         <Sidebar />
 
-        <main className="flex-1 p-6 overflow-y-auto ml-64 my-10">
+        <main className="flex-1 bg-gray-50 p-6 overflow-y-auto ml-64 my-10">
           <Outlet />
         </main>
       </div>
--- src/pages/company/Dashboard.jsx
+++ src/pages/company/Dashboard.jsx
@@ -9,7 +9,7 @@ import { FaUsers, FaClipboardCheck, FaCalendarCheck } from "react-icons/fa";
 
 const Dashboard = () => {
   return (
-    <div className="p-6 space-y-6">
+    <div className="p-6 space-y-6 ">
       {/* KPI Boxes */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <KPIBox title="Total Requests" value="124" icon={<FaUsers />} />
@@ -27,6 +27,7 @@ const Dashboard = () => {
         </div>
       </div>
 
+      {/* Recent Requests and Upcoming Schedule */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <RecentRequestsList />
         <UpcomingSchedule />
