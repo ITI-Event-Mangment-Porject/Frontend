78faf73b24900e892b6e2a4d34e4d907b29887f3
Ghada Emad <ghadaemad011@gmail.com>
2025-06-19 00:26:42 +0300

    add jobfair setup for companies

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import UserManagement from '../pages/admin/UserManagement';
+import JobFairSetup from '../pages/admin/JobFairSetup';
 
 // Admin Pages
 // import AdminDashboard from '../pages/admin/Dashboard';
@@ -11,14 +12,14 @@ const AppRoutes = () => {
       {/* Admin routes - using absolute paths */}
       {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
       <Route path="/admin/users" element={<UserManagement />} />
-
+      <Route path="/admin/job-fair" element={<JobFairSetup />} />
       {/* Default redirect to admin dashboard */}
       <Route
         path="/admin"
         element={<Navigate to="/admin/dashboard" replace />}
       />
       <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
-
+     
       {/* Catch all route */}
       <Route path="*" element={<Navigate to="/admin/dashboard" replace />} />
     </Routes>
