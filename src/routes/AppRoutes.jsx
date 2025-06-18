4f492b0a05b366ee90d19de578dea3f6e37dab18
Ali Elgendy <aliahmedelgendy2022@gmail.com>
2025-06-18 05:49:59 +0300

    update routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,13 +3,13 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import UserManagement from '../pages/admin/UserManagement';
 
 // Admin Pages
-import AdminDashboard from '../pages/admin/Dashboard';
+// import AdminDashboard from '../pages/admin/Dashboard';
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Admin routes - using absolute paths */}
-      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
       <Route path="/admin/users" element={<UserManagement />} />
 
       {/* Default redirect to admin dashboard */}
