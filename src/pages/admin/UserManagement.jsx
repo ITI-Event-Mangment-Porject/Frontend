35eba2f10ac060af6b7eb7433504c590aad05e1a
“[Noor <nourmamoun@gmail.com>
2025-06-18 12:58:37 +0300

    feat: Add useScrollToTop hook and integrate it into UserManagement component

--- src/pages/admin/UserManagement.jsx
+++ src/pages/admin/UserManagement.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import UserManagementComponent from '../../components/admin/User Managment Components/UserManagemntComponentRefactored';
 import Layout from '../../components/common/Layout';
+import useScrollToTop from '../../hooks/useScrollToTop';
 
 const UserManagement = () => {
+  // Use our custom hook to scroll to top when component mounts
+  useScrollToTop();
+
   return (
     <Layout>
       <div className="min-h-screen py-6">
